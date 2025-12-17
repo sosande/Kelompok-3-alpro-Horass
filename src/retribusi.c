25442a8923fa71e897ded153e838541f08d5946c
Yasamu foryou <ilyasa.udo@gmail.com>
2025-12-18 05:29:28 +0700

    pembayan

--- src/retribusi.c
+++ src/retribusi.c
@@ -1,61 +1,137 @@
 #include <stdio.h>
 #include <string.h>
-#include "../include/structs.h"
-#include "../include/utils.h"
-#include "../include/retribusi.h"
 
-extern Penduduk dataWarga[MAX_WARGA];
-extern int jumlahWarga;
-extern Transaksi dataTransaksi[MAX_TRANSAKSI];
-extern int jumlahTransaksi;
+#define MAX_WARGA 100
+#define MAX_TRANSAKSI 100
 
-void bayarRetribusi() {
-    // TODO: Tugas
-    // 1. Cari NIK pembayar
-    // 2. Input Jenis (Kebersihan/Keamanan) & Nominal
-    // 3. Simpan ke array dataTransaksi
-    
-    printf("\n[DEV] Fitur Bayar Retribusi belum diisi.\n");
-    jedaLayar();
-}
+/* =====================
+   STRUCT DATA
+===================== */
+typedef struct {
+    int id;
+    char nama[50];
+    int rt;
+} Warga;
 
-void laporanTunggakan() {
-    // TODO: Tugas 
-    // 1. Identifikasi siapa yang belum ada di dataTransaksi bulan ini
-    // 2. Masukkan mereka ke array sementara
-    // 3. Lakukan Bubble Sort / Selection Sort berdasarkan Nama/RT
-    // 4. Tampilkan daftarnya
-    
-    printf("\n[DEV] Fitur Laporan Tunggakan (Sorting) belum diisi.\n");
-    jedaLayar();
-}
+typedef struct {
+    int idWarga;
+    int nominal;
+    char tanggal[20];
+} Retribusi;
 
-void riwayatTransaksi() {
-    // TODO: Tugas 
-    // Tampilkan tabel semua uang yang masuk
-    
-    printf("\n[DEV] Fitur Riwayat Transaksi belum diisi.\n");
-    jedaLayar();
-}
+/* =====================
+   GLOBAL DATA (SIMULASI)
+===================== */
+Warga dataWarga[MAX_WARGA];
+Retribusi dataRetribusi[MAX_TRANSAKSI];
 
-void menuRetribusi() {
-    int pilihan;
-    do {
-        bersihkanLayar();
-        printf("=== LOKET RETRIBUSI ===\n");
-        printf("[1] Bayar Retribusi\n");
-        printf("[2] Cek Penunggak\n");
-        printf("[3] Riwayat Transaksi Masuk\n");
-        printf("[0] Kembali\n");
-        printf("Pilihan: ");
-        scanf("%d", &pilihan);
+int jumlahWarga = 0;
+int jumlahRetribusi = 0;
 
-        switch(pilihan) {
-            case 1: bayarRetribusi(); break;
-            case 2: laporanTunggakan(); break;
-            case 3: riwayatTransaksi(); break;
-            case 0: break;
-            default: printf("Salah input!\n"); jedaLayar();
+/* =====================
+   FUNGSI BANTUAN
+===================== */
+int sudahBayar(int idWarga) {
+    for (int i = 0; i < jumlahRetribusi; i++) {
+        if (dataRetribusi[i].idWarga == idWarga) {
+            return 1;
         }
-    } while (pilihan != 0);
+    }
+    return 0;
+}
+
+/* =====================
+   BAYAR RETRIBUSI
+===================== */
+void bayarRetribusi() {
+    int id, nominal;
+    char tanggal[20];
+
+    printf("\n=== BAYAR RETRIBUSI ===\n");
+    printf("Masukkan ID Warga   : ");
+    scanf("%d", &id);
+
+    printf("Masukkan Nominal    : ");
+    scanf("%d", &nominal);
+
+    printf("Masukkan Tanggal    : ");
+    scanf("%s", tanggal);
+
+    dataRetribusi[jumlahRetribusi].idWarga = id;
+    dataRetribusi[jumlahRetribusi].nominal = nominal;
+    strcpy(dataRetribusi[jumlahRetribusi].tanggal, tanggal);
+
+    jumlahRetribusi++;
+
+    printf(">> Pembayaran berhasil dicatat.\n");
+}
+
+/* =====================
+   SORT BUBBLE (A-Z)
+===================== */
+void sortPenunggak(Warga penunggak[], int n) {
+    Warga temp;
+    for (int i = 0; i < n - 1; i++) {
+        for (int j = 0; j < n - i - 1; j++) {
+            if (strcmp(penunggak[j].nama, penunggak[j + 1].nama) > 0) {
+                temp = penunggak[j];
+                penunggak[j] = penunggak[j + 1];
+                penunggak[j + 1] = temp;
+            }
+        }
+    }
+}
+
+/* =====================
+   LAPORAN PENUNGGAK
+===================== */
+void laporanPenunggak() {
+    Warga penunggak[MAX_WARGA];
+    int jumlahPenunggak = 0;
+
+    for (int i = 0; i < jumlahWarga; i++) {
+        if (!sudahBayar(dataWarga[i].id)) {
+            penunggak[jumlahPenunggak++] = dataWarga[i];
+        }
+    }
+
+    sortPenunggak(penunggak, jumlahPenunggak);
+
+    printf("\n=== LAPORAN PENUNGGAK RETRIBUSI ===\n");
+    printf("ID\tNama\t\tRT\n");
+
+    for (int i = 0; i < jumlahPenunggak; i++) {
+        printf("%d\t%-15s\t%d\n",
+               penunggak[i].id,
+               penunggak[i].nama,
+               penunggak[i].rt);
+    }
+
+    if (jumlahPenunggak == 0) {
+        printf("Tidak ada penunggak.\n");
+    }
+}
+
+/* =====================
+   MENU RETRIBUSI
+===================== */
+void menuRetribusi() {
+    int pilih;
+    do {
+        printf("\n=== MENU RETRIBUSI ===\n");
+        printf("1. Bayar Retribusi\n");
+        printf("2. Laporan Penunggak\n");
+        printf("0. Kembali\n");
+        printf("Pilih: ");
+        scanf("%d", &pilih);
+
+        switch (pilih) {
+            case 1:
+                bayarRetribusi();
+                break;
+            case 2:
+                laporanPenunggak();
+                break;
+        }
+    } while (pilih != 0);
 }
